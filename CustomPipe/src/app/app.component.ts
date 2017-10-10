35eb3a3aa3405b1066f879ff41594e54f65fb269
Alicia Serrano Sánchez <alisesan86@gmail.com>
2017-10-10 18:26:51 +0200

    Tubes

--- CustomPipe/src/app/app.component.ts
+++ CustomPipe/src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  value: number = 23.56782;
+  cadena: Array<string> = ['a','b','c', 'd','e','f'];
+  fecha: number = Date.now();
+  fecha2: Date = new Date(2017,9,33);
+  precio: number = 29.99;
+  object: Object = {foo: 'bar', baz: 'qux', nested: {xyz: 3, numbers: [1, 2, 3, 4, 5]}};
+  nombre: string = 'Alicia';
+  numero: number = 0.2345;
 }

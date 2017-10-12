c62fe78061ec3f329357cc6fa39c4e485dca5cc2
Alicia Serrano Sánchez <alisesan86@gmail.com>
2017-10-12 19:12:08 +0200

    Routes in angular

---
+++ RoutesProject/src/app/hello/hello.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+
+@Component({
+  selector: 'app-hello',
+  templateUrl: './hello.component.html',
+  styleUrls: ['./hello.component.css']
+})
+export class HelloComponent implements OnInit {
+
+  name: string;
+
+  constructor(private route: ActivatedRoute,
+  private router: Router) { }
+  ngOnInit() {
+    this.name = this.route.snapshot.params['name'];
+  }
+
+}

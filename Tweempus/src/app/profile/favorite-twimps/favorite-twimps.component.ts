bf8d68dce60eccecebb3522025d246dbc392e655
Alicia Serrano Sánchez <alisesan86@gmail.com>
2017-11-18 13:41:26 +0100

    My favorites twimps finished

--- Tweempus/src/app/profile/favorite-twimps/favorite-twimps.component.ts
+++ Tweempus/src/app/profile/favorite-twimps/favorite-twimps.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, Params } from '@angular/router';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/from';
+
+import { TwimpService } from '../../shared/twimp/twimp.service';
+import { AuthorService } from '../../shared/author/author.service';
+
+import { Twimp } from '../../shared/twimp/twimp.model';
+import { Route } from '@angular/router/src/config';
 
 @Component({
   selector: 'tweempus-favorite-twimps',
@@ -7,9 +17,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavoriteTwimpsComponent implements OnInit {
 
-  constructor() { }
+  myfavoritestwimps: Twimp[] = [];
+  idAuthor: string = null;
+
+  constructor(private twimpService: TwimpService,
+            private authorService: AuthorService,
+            private route: ActivatedRoute,
+            private router: Router) { }
 
   ngOnInit() {
+    this.idAuthor = this.route.parent.snapshot.params['id'];
+    this.twimpService.getTwimps().subscribe(twimps => {
+      Observable.from(twimps).subscribe(twimp => {
+        this.twimpService.isFavoriteByAuthor(this.idAuthor, twimp.id).subscribe(favorite =>{
+          if (favorite) {
+            twimp.favorite = favorite;
+            this.authorService.getAuthor(twimp.author.id).subscribe(author =>{
+              twimp.author = author;
+              this.myfavoritestwimps.push(twimp);
+            });
+          }
+        });
+      });
+
+    });
   }
 
 }

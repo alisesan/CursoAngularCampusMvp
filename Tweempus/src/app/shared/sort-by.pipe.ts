ab6c71e7bbe521faeb6ad7572a4b6b1ab04142d1
Alicia Serrano Sánchez <alisesan86@gmail.com>
2017-11-18 16:27:18 +0100

    Fixed pipe to sort twimps list by date

--- Tweempus/src/app/shared/sort-by.pipe.ts
+++ Tweempus/src/app/shared/sort-by.pipe.ts
@@ -10,7 +10,15 @@ export class SortByPipe implements PipeTransform {
 
   transform(array: Array<Twimp>): any {
     if (array) {
-      return array.sort((a: Twimp, b: Twimp) => +a.timestamp.replace(/-/g, '') - +b.timestamp.replace(/-/g, ''));
+      // .replace(/\//g, '-')
+      var pattern = /(\d{2})\/(\d{2})\/(\d{4})/;
+
+      return array.sort((a: Twimp, b: Twimp) => {
+        var dt1 = new Date(a.timestamp.replace(/-/g, '/').replace(pattern, '$3-$2-$1'));
+        var dt2 = new Date(b.timestamp.replace(/-/g, '/').replace(pattern, '$3-$2-$1'));
+        return +dt2 - +dt1;
+      });
+      //return array.sort((a: Twimp, b: Twimp) => +a.timestamp.replace(/-/g, '') - +b.timestamp.replace(/-/g, ''));
     }
     else {
       return array;
